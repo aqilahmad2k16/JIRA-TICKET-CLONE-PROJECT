3215219017bd7b9941f3d7121bd357a7c59cd42d
Aqil Ahmad <er.aqilahmad031@gmail.com>
2022-01-27 20:45:10 +0530

    new change has made by me today

--- JIRA TICKET CLONE/script.js
+++ JIRA TICKET CLONE/script.js
@@ -1,1 +1,19 @@
-console.log("hi this is Aqil Ahmad");
+let addbtn = document.querySelector('.add-btn');
+let modalbox = document.querySelector(".modal-cont");
+let addflag = false;
+
+addbtn.addEventListener("click", ()=>{
+    //Display modal
+    //generate ticket;
+
+    //addflag -> true :=> display modal;
+    // addflag -> false :=> modal none
+
+    addflag = !addflag;
+    if(addflag){
+        modalbox.style.display = "flex";
+    }else{
+        modalbox.style.display = 'none';
+    }
+
+})
